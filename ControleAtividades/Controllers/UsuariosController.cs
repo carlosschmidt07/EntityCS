c8fd2a2dd8eb37009974f29de4e550119fd10c7f
Carlos Eduardo Egea Schmidt <carlos_schmidt07@hotmail.com>
2018-06-04 20:14:50 -0300

    aula 04/06

--- ControleAtividades/Controllers/UsuariosController.cs
+++ ControleAtividades/Controllers/UsuariosController.cs
@@ -1,6 +1,42 @@
-﻿namespace Controllers
+﻿using System.Collections.Generic;
+using Controllers.Base;
+using Controllers.DAL;
+using Modelos;
+
+namespace Controllers
 {
-    public class UsuariosController
+    public class UsuariosController : IBaseController<Usuario>
     {
+        private Contexto contexto = new Contexto();
+        public void Adicionar(Usuario entity)
+        {
+            contexto.Usuarios.Add(entity);
+            contexto.SaveChanges();
+        }
+
+        public Usuario BuscarPorID(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Editar(Usuario entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Usuario> ListarPorNome()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Usuario> ListarTodos()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
